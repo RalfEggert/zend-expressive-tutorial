53a3a4e643d669f299b18fda73f4ac9c6cd24f5d
Ralf Eggert <ralf@travello.com>
2015-12-10 15:00:32 +0100

    Removed unused app template dir config

--- config/autoload/templates.global.php
+++ config/autoload/templates.global.php
@@ -22,7 +22,6 @@ return [
             'error/404'      => 'templates/error/404.phtml',
         ],
         'paths' => [
-            'app'    => ['templates/app'],
             'layout' => ['templates/layout'],
             'error'  => ['templates/error'],
         ],
