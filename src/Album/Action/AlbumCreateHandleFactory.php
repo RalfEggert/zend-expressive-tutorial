f7fecdfd4be326b88a3ccc36b6344e30c204a3f8
Ralf Eggert <ralf@travello.com>
2015-12-18 10:27:55 +0100

    Fixed bug in create form handling factory

--- src/Album/Action/AlbumCreateHandleFactory.php
+++ src/Album/Action/AlbumCreateHandleFactory.php
@@ -21,13 +21,12 @@ class AlbumCreateHandleFactory
      */
     public function __invoke(ContainerInterface $container)
     {
-        $template        = $container->get(TemplateRendererInterface::class);
         $router          = $container->get(RouterInterface::class);
         $albumRepository = $container->get(AlbumRepositoryInterface::class);
         $albumForm       = $container->get(AlbumDataForm::class);
 
         return new AlbumCreateHandleAction(
-            $template, $router, $albumRepository, $albumForm
+            $router, $albumRepository, $albumForm
         );
     }
 }
